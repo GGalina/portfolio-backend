c2a8a46ab5b1773771a5ee07db97189d075d1c15
Halyna Hryn <71042425+GGalina@users.noreply.github.com>
2026-03-01 17:59:09 +0200

    UPDATE: For deploy

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

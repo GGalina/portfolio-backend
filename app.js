3009d97e86be6de3ac7a76f839a90106ee013900
Halyna Hryn <71042425+GGalina@users.noreply.github.com>
2026-03-01 18:04:39 +0200

    UPDATE: For testing

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
 app.use('/', mailRouter);
 
 app.use((req, res) => {
